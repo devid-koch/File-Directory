16b18067bfee499fa47950f4a2f3e721c36ce5a6
deviid123 <devidkoch321@gmail.com>
2024-11-07 11:42:45 +0530

    connect online database

--- file-directory-backend/config/db.js
+++ file-directory-backend/config/db.js
@@ -1,10 +1,23 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST || 'localhost',
+// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+//     host: process.env.DB_HOST || 'localhost',
+//     dialect: process.env.DB_DIALECT || 'postgres',
+//     port: process.env.DB_PORT || 5432,
+// });
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: process.env.DB_DIALECT || 'postgres',
-    port: process.env.DB_PORT || 5432,
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
+        },
+    },
 });
+
+module.exports = sequelize;
+
 
 module.exports = sequelize;
